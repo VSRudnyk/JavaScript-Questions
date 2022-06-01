739901d37eca3f7cf26ed0033d21d3c0d0437642
Vladimir Rudnyk <v.rudnyk@gmail.com>
2022-06-01 22:01:08 +0300

    start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { useState } from 'react';
+import data from '../data/data.json';
+
 export const App = () => {
+  const id = 1;
+  const currentCard = data.find(card => card.id === String(id));
+  const { question, codeExample, possiblAnswer, correctAnswer } = currentCard;
+  const [disabled, setDisabled] = useState(false);
+
+  const handelClick = e => {
+    const selectedAnswer = e.target.dataset.answer;
+    if (selectedAnswer === correctAnswer) {
+      e.target.classList.add('green');
+      setDisabled(true);
+    } else {
+      e.target.classList.add('red');
+      setDisabled(true);
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h2>{question}</h2>
+      <pre>
+        <code>{codeExample}</code>
+      </pre>
+      {possiblAnswer.map((answer, index) => (
+        <button
+          onClick={handelClick}
+          disabled={disabled}
+          data-answer={answer}
+          key={index}
+        >
+          {answer}
+        </button>
+      ))}
     </div>
   );
 };
